b592570015d473d861a1130ceb6f95fdecbb1ce7
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2023-12-31 16:27:34 +0700

    update the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,48 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
-import React from 'react' 
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
 
-export const metadata = {"title":"Ask a Question","id":"ask-question","layout":"base","tags":["ask","question"],"description":"Ask a question and get answers from the community.","category":"question","rank":1,"slug":"ask-question"}
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Ask a Question",
+  description: "Ask a question and get answers from the community.",
+  icons: {
+    icon: "assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider 
+     appearance={{
+      elements : {
+        formButtonPrimary:'primary-gradient',
+        footerActionLink:'primary-text-gradient hover:text-primary-500',
+      }
+     }} 
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
