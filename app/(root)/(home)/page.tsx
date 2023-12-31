b3269ec122be9f0791e25a09a3a2ffeb89d203f1
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2023-12-31 15:07:44 +0700

    add auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,9 @@
-const Home = () => {
+import { UserButton } from "@clerk/nextjs";
+ 
+export default function Home() {
   return (
-    <div>
-      <h1 className="h1-bold">Hello World</h1>
-      <h1 className="h2-bold">Hello World</h1>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
     </div>
-  );
-} 
-export default Home
+  )
+}
